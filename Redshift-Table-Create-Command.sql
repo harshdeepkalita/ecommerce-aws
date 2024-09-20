7a9dadd8690c303edd3560d8690f5135e1cee2ea
Harsh Deep Kalita <96279045+harshdeepkalita@users.noreply.github.com>
2024-09-20 18:46:10 +0100

    Create Redshift-Table-Create-Command.sql

---
+++ Redshift-Table-Create-Command.sql
@@ -0,0 +1,10 @@
+create table firehosetransactions(
+	InvoiceNo int not null,
+	StockCode varchar(200) not null,
+	Description varchar(200) not null,
+	Quantity int not null,	
+	InvoiceDate varchar(200) not null,
+	UnitPrice float not null,
+	CustomerID int not null,  	
+ 	Country varchar(200) not null
+);
